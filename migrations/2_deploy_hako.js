cd7947af5181abd644d25b3441a99e79bb2d0585
okada-shun <syun19981207@gmail.com>
2021-06-05 19:40:54 +0900

    Change the initialSupply from 1000 to 50000

--- migrations/2_deploy_hako.js
+++ migrations/2_deploy_hako.js
@@ -1,10 +1,10 @@
 const Hako = artifacts.require("./Hako.sol");
 
 module.exports = function(deployer) {
-  const initialSupply = 1000;
+  const initialSupply = 50000;
   const initialUpperLimit = 500;
-  const hakoName = "HakoPrototype";
-  const hakoSymbol = "HKPT";
+  const hakoName = "HakoTest";
+  const tokenSymbol = "HTKN";
   const decimals = 0;
-  deployer.deploy(Hako, initialSupply, initialUpperLimit, hakoName, hakoSymbol, decimals);
+  deployer.deploy(Hako, initialSupply, initialUpperLimit, hakoName, tokenSymbol, decimals);
 };
