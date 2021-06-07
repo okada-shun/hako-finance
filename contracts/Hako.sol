f31dda7f893f2af060c4e6c8eb59c06dcad3626d
okada-shun <syun19981207@gmail.com>
2021-06-07 12:39:37 +0900

    Rename hakoName to tokenName

--- contracts/Hako.sol
+++ contracts/Hako.sol
@@ -4,24 +4,24 @@ pragma solidity >=0.4.22 <0.9.0;
 import "./CreditCreation.sol";
 
 ///@title Hako
+///@notice hakoAddress is this contract's address.
 contract Hako is CreditCreation {
 
   string public name;
   string public symbol;
   uint public decimals;
   
-  ///@notice hakoAddress is this contract's address.
   ///@param initialSupply Initial supply of hako token.
   ///@param initialUpperLimit Initial upper limit of borrowing and credit-creation value.
-  ///@param hakoName Hako's name.
+  ///@param tokenName Hako token's name.
   ///@param tokenSymbol Hako token's symbol.
   ///@param _decimals decimals of hako token.
-  constructor(uint256 initialSupply, uint256 initialUpperLimit, string memory hakoName, string memory tokenSymbol, uint256 _decimals) public {
+  constructor(uint256 initialSupply, uint256 initialUpperLimit, string memory tokenName, string memory tokenSymbol, uint256 _decimals) public {
     hakoOwner = msg.sender;
     hakoAddress = address(this);
     totalSupply_ = initialSupply;
     upperLimit = initialUpperLimit;
-    name = hakoName;
+    name = tokenName;
     symbol = tokenSymbol;
     decimals = _decimals;
     balances[hakoOwner] = initialSupply;
--- migrations/2_deploy_hako.js
+++ migrations/2_deploy_hako.js
@@ -3,8 +3,8 @@ const Hako = artifacts.require("./Hako.sol");
 module.exports = function(deployer) {
   const initialSupply = 50000;
   const initialUpperLimit = 500;
-  const hakoName = "HakoTest";
+  const tokenName = "HakoToken";
   const tokenSymbol = "HTKN";
   const decimals = 0;
-  deployer.deploy(Hako, initialSupply, initialUpperLimit, hakoName, tokenSymbol, decimals);
+  deployer.deploy(Hako, initialSupply, initialUpperLimit, tokenName, tokenSymbol, decimals);
 };
